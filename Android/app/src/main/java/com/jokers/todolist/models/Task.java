d9e2d4616d9e4d7b10d6b60791a44947b8a5c737
phuanh004 <phuanh004@gmail.com>
2020-10-24 15:35:22 -0700

    Reformat Task.java class
    Fix Tag.class has not attached
    Add default constructor
    Remove setTags() and getTags()
    
    Reformat code

--- Android/app/src/main/java/com/jokers/todolist/models/Task.java
+++ Android/app/src/main/java/com/jokers/todolist/models/Task.java
@@ -1,11 +1,14 @@
 package com.jokers.todolist.models;
-import java.util.ArrayList;
 
-/** 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
  * Task class
  * Purpose: Task class initialized by user class;
+ *
  * @author Guan Li
-*/
+ */
 
 public class Task {
     //User's two attribute:
@@ -14,43 +17,79 @@ public class Task {
     private String description;
     private int createdDate;
     private int dueDate;
-    private ArrayList<Tags> tags = new ArrayList<Tags>();
+    private List<Tag> tags = null;
 
     //Constructor:
-    Task(String id, String title, String description, int createdDate, int dueDate){ //Constructor:
+    Task() {
+        tags = new ArrayList<>();
+    }
+
+    Task(String id, String title, String description, int createdDate, int dueDate) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.createdDate = createdDate;
         this.dueDate = dueDate;
     }
-    public int getTimeRemain(){return this.dueDate -  this.createdDate;}
-    //getter and setter
-    // private String id;
-    // private String title;
-    // private String description;
-    // private int createdDate;
-    // private int dueDate;
-    // private ArrayList<Tags> tags = new ArrayList<Tags>();
-    public String getID(){return this.id;}
-    public String getTitle(){return this.title;}
-    public String getDescription(){return this.description;}
-    public int getCreatedDate(){return this.createdDate;}
-    public int getDueDate(){return this.dueDate;}
-    
-    public Tags[] getTags(){
-        Tags[] temp = new Tags[tags.size()];
-        for (int i = 0; i < tags.size(); i++){
-            temp[i] = tags.get(i);
-        }
-        return temp;
+
+    //Getters:
+    public int getTimeRemain() {
+        return this.dueDate - this.createdDate;
     }
+
+    public String getID() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getCreatedDate() {
+        return this.createdDate;
+    }
+
+    public int getDueDate() {
+        return this.dueDate;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
     //Setters:
-    public void setID(String id){this.id = id;}
-    public void setTitle(String title){this.title = title;}
-    public void setDescription(String description){this.description = description;}
-    public void setCreatedDate(int createdDate){this.createdDate = createdDate;}
-    public void setDueDate(int dueDate){this.dueDate = dueDate;}
-    public void setTags(Tags tag, int position){
-        for (int i = 0; i < tags.size(); i++ ){if (i == position){tags.set(i,tags);}}}
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreatedDate(int createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setDueDate(int dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    /**
+     * Set tag to the tag list
+     * @param position  Set tag at position
+     * @param id        Tag id
+     * @param text      Tag text
+     */
+    public void setTagAt(int position, String id, String text) {
+        Tag tag = new Tag(id, text);
+        tags.set(position, tag);
+    }
 }
