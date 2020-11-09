db39e32593773f9d226955b3a45d57c5e6df15f6
trungtinvo <72297358+trungtinvo@users.noreply.github.com>
2020-11-08 17:47:56 -0800

    Add retrieved data

--- Android/app/src/main/java/com/jokers/todolist/MainActivity.java
+++ Android/app/src/main/java/com/jokers/todolist/MainActivity.java
@@ -2,20 +2,27 @@ package com.jokers.todolist;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.jokers.todolist.models.ToDo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
     // Declare an instance of FirebaseAuth
     private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
+    private DatabaseReference reference;
+    private String onlineUserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,10 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        mUser = mAuth.getCurrentUser();
+        onlineUserID = mUser.getUid();
+        reference = FirebaseDatabase.getInstance().getReference().child("tasks").child(onlineUserID);
     }
 
     @Override
@@ -60,6 +75,46 @@ public class MainActivity extends AppCompatActivity {
         else {
             Intent loginIntent = new Intent(this, LoginActivity.class);
             startActivity(loginIntent);
+        }
+
+        FirebaseRecyclerOptions<ToDo> options = new FirebaseRecyclerOptions.Builder<ToDo>()
+                .setQuery(reference, ToDo.class)
+                .build();
+
+        FirebaseRecyclerAdapter<ToDo, MyViewHolder> adapter = new FirebaseRecyclerAdapter<ToDo, MyViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull ToDo toDo) {
+                holder.setDueDate(toDo.getDueDate());
+                holder.setTitle(toDo.getTitle());
+                holder.setDescription(toDo.getDescription());
+            }
+
+            @NonNull
+            @Override
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.retrieved_layout,parent, false);
+                return new MyViewHolder(view);
+            }
+        };
+
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+        public void setTitle (String title) {
+            TextView taskTextView = mView.findViewById(R.id.titleTv);
+            taskTextView.setText(title);
+        }
+        public void setDescription(String description) {
+            TextView descriptionTextView = mView.findViewById(R.id.descriptionTv);
+            descriptionTextView.setText(description);
+        }
+        public void setDueDate(String dueDate) {
+            TextView dateTextView = mView.findViewById(R.id.dueDateTv);
         }
     }
 
