31f7bf29ae192cad9194faae28ba22b5ac8d1cdd
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2020-12-13 14:16:19 -0800

    NightMode Completed
    Update: All text colors, all buttons, and included nightmode feature.
    
    Issue: The Toggle swtich bar does not save.

--- Android/app/src/main/java/com/jokers/todolist/fragments/SettingFragment.java
+++ Android/app/src/main/java/com/jokers/todolist/fragments/SettingFragment.java
@@ -7,10 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,6 +32,7 @@ public class SettingFragment extends Fragment implements FirebaseAuth.AuthStateL
     // Declare view ids
     private Button mLogOutButton;
     private TextView mUserNameTextView;
+    private Switch nightModeSwt;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -46,10 +51,25 @@ public class SettingFragment extends Fragment implements FirebaseAuth.AuthStateL
             if (Objects.requireNonNull(mAuth.getCurrentUser()).isAnonymous()) {
                 mAuth.getCurrentUser().delete();
             }
-
             mAuth.signOut();
         });
+
+
+
+        nightModeSwt = requireView().findViewById(R.id.nightModeSwitch);
+
+        nightModeSwt.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (nightModeSwt.isChecked()) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+        });
     }
+
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -76,7 +96,7 @@ public class SettingFragment extends Fragment implements FirebaseAuth.AuthStateL
         }
 
         // If not, navigate user to LoginActivity
-        else {
+        else if (getContext() != null){
             Intent loginIntent = new Intent(getContext(), LoginActivity.class);
             startActivity(loginIntent);
         }
